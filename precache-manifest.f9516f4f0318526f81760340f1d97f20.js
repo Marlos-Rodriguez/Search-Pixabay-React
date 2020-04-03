cf9106cb855200b22f8725211ebfaa3d05325dbc
Marlos <Marlos2811@hotmail.com>
2020-04-03 16:26:46 -0300

    gh-pages Changes

---
+++ precache-manifest.f9516f4f0318526f81760340f1d97f20.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c838939be0fe19f6ac1a13adb62457c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f85af51c7b4873778cfe",
+    "url": "./static/js/2.e1d071f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.e1d071f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5e49884c4b8c997a9e49",
+    "url": "./static/js/main.ba2a7d7e.chunk.js"
+  },
+  {
+    "revision": "40a641067b9ba72654fe",
+    "url": "./static/js/runtime-main.0f655198.js"
+  }
+]);
